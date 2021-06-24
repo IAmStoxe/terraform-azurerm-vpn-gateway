31e0ed9ef2b6fb0b77fc31aea0ee2b917efa7d6e
Devin Stokes <rune.dev@gmail.com>
2021-06-24 13:33:01 -0700

    added location for resource group usage

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@
 #----------------------------------------------------------
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
+  location = var.location
 }
 
 data "azurerm_virtual_network" "vnet" {
